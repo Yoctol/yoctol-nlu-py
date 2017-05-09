03b645eff800832fdf2af5b3d92548c5118af06e
cph <stegben.benjamin@gmail.com>
2017-05-09 16:41:36 +0800

    intent classifier structure

--- ynlu/__init__.py
+++ ynlu/__init__.py
@@ -1,1 +1,3 @@
 __version__ = '0.1.0'
+
+from .intent_classifier import client as IntentClassifierClient
---
+++ ynlu/test/test_import.py
@@ -0,0 +1,2 @@
+def test_import_intent_classifier_client():
+    from .. import IntentClassifierClient
