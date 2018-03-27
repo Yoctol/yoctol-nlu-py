9f81b753ab01110320d078445bfdcf288e1a5b30
Solumilken <s916526000@gmail.com>
2018-03-27 15:34:35 +0800

    calculate accuracy by one instance or all

--- ynlu/sdk/evaluation/tests/test_intent_topk_accuracy_score.py
+++ ynlu/sdk/evaluation/tests/test_intent_topk_accuracy_score.py
@@ -1,10 +1,136 @@
 from unittest import TestCase
-from ..intent_topk_accuracy_score import intent_topk_accuracy_score
+from ..intent_topk_accuracy_score import (
+    intent_topk_accuracy_score,
+    intent_topk_accuracy_score_overall,
+)
 
 
 class TopKAccuracyScoreTestCase(TestCase):
 
     def test_top1_accuracy_score(self):
+        test_cases = [
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["1", "3"],
+                    "k": 1,
+                },
+                0.499999999999,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["1"],
+                    "k": 1,
+                },
+                0.999999999999,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["1", "3", "7", "5"],
+                    "k": 1,
+                },
+                0.25,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["5"],
+                    "k": 1,
+                },
+                0.0,
+            ),
+        ]
+        for i, test_case in enumerate(test_cases):
+            with self.subTest(i=i):
+                result = intent_topk_accuracy_score(**test_case[0])
+                self.assertAlmostEqual(test_case[1], result)
+
+    def test_top3_accuracy_score(self):
+        test_cases = [
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["1", "3"],
+                    "k": 3,
+                },
+                0.6666666666667,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["1"],
+                    "k": 3,
+                },
+                0.3333333333333,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["1", "3", "7", "5"],
+                    "k": 3,
+                },
+                0.39999999999999,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                        {"name": "3"},
+                    ],
+                    "y_true": ["5"],
+                    "k": 3,
+                },
+                0.0,
+            ),
+            (
+                {
+                    "intent_prediction": [
+                        {"name": "1"},
+                        {"name": "2"},
+                    ],
+                    "y_true": ["1", "3", "7", "5"],
+                    "k": 3,
+                },
+                0.19999999999999,
+            ),
+        ]
+        for i, test_case in enumerate(test_cases):
+            with self.subTest(i=i):
+                result = intent_topk_accuracy_score(**test_case[0])
+                self.assertAlmostEqual(test_case[1], result)
+
+    def test_top1_accuracy_score_overall(self):
         test_cases = [
             (
                 {
@@ -33,5 +159,34 @@ class TopKAccuracyScoreTestCase(TestCase):
         ]
         for i, test_case in enumerate(test_cases):
             with self.subTest(i=i):
-                result = intent_topk_accuracy_score(**test_case[0])
+                result = intent_topk_accuracy_score_overall(**test_case[0])
+                self.assertAlmostEqual(test_case[1], result)
+
+    def test_top3_accuracy_score_overall(self):
+        test_cases = [
+            (
+                {
+                    "intent_predictions": [
+                        [
+                            {"name": "1"},
+                            {"name": "2"},
+                        ],
+                        [
+                            {"name": "2"},
+                            {"name": "4"},
+                            {"name": "6"},
+                        ],
+                        [
+                            {"name": "7"},
+                        ],
+                    ],
+                    "y_trues": [["1"], ["3"], ["7"]],
+                    "k": 3,
+                },
+                0.49999999999999,
+            ),
+        ]
+        for i, test_case in enumerate(test_cases):
+            with self.subTest(i=i):
+                result = intent_topk_accuracy_score_overall(**test_case[0])
                 self.assertAlmostEqual(test_case[1], result)
