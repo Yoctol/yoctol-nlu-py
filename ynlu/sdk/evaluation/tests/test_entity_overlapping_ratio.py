0193e4ce9d075582701435def8cd76fee9fd7ee3
Solumilken <s916526000@gmail.com>
2018-03-28 17:26:00 +0800

    batch version overlapping scores

--- ynlu/sdk/evaluation/tests/test_entity_overlapping_ratio.py
+++ ynlu/sdk/evaluation/tests/test_entity_overlapping_ratio.py
@@ -1,10 +1,24 @@
 from unittest import TestCase
-from ..entity_overlapping_score import entity_overlapping_score
+from ..entity_overlapping_score import (
+    single__entity_overlapping_score,
+    entity_overlapping_score,
+)
 
 
 class OverlappingScoreTestCase(TestCase):
 
-    def test_evaluate(self):
+    def test_single__entity_overlapping_score_different_length(self):
+        with self.assertRaises(ValueError):
+            single__entity_overlapping_score(
+                utterance="12",
+                entity_prediction=[
+                    {"value": "1", "entity": "a"},
+                    {"value": "2", "entity": "b"},
+                ],
+                y_true=["a", "b", "c"],
+            )
+
+    def test_single__entity_overlapping_score(self):
         test_cases = [
             (
                 {
@@ -128,5 +142,38 @@ class OverlappingScoreTestCase(TestCase):
         ]
         for i, test_case in enumerate(test_cases):
             with self.subTest(i=i):
-                result = entity_overlapping_score(**test_case[0])
+                result = single__entity_overlapping_score(**test_case[0])
                 self.assertAlmostEqual(test_case[1], result)
+
+    def test_entity_overlapping_score_different_amount(self):
+        with self.assertRaises(ValueError):
+            entity_overlapping_score(
+                utterances=["123", "345"],
+                entity_predictions=[[{"a": 1}], [{"b": 2}]],
+                y_trues=[["a"], ["b"], ["c"]],
+            )
+
+    def test_entity_overlapping_score(self):
+        result = entity_overlapping_score(
+            utterances=["123", "123"],
+            entity_predictions=[
+                [
+                    {"entity": "1", "value": "1"},
+                    {"entity": "2", "value": "2"},
+                    {"entity": "3", "value": "3"},
+                ],
+                [
+                    {"entity": "DONT_CARE", "value": "1"},
+                    {"entity": "DONT_CARE", "value": "2"},
+                    {"entity": "DONT_CARE", "value": "3"},
+                ],
+            ],
+            y_trues=[
+                ["5", "2", "3"],
+                ["DONT_CARE", "DONT_CARE", "DONT_CARE"],
+            ],
+        )
+        self.assertAlmostEqual(
+            (0.33333333333 + 1.0) / 2,
+            result,
+        )
