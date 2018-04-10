7aa8324caca7be88a86ef3d1b9f530c677d21bff
Solumilken <s916526000@gmail.com>
2018-04-10 15:17:22 +0800

    more docstring

--- ynlu/sdk/evaluation/plot/utils.py
+++ ynlu/sdk/evaluation/plot/utils.py
@@ -28,6 +28,13 @@ def _get_all_text_objects(obj):
 def plt_set_font_style(font_style_path: str = None):
     """Setting font style of figure plotting by matplotlib
 
+        By calling the function `plt_set_font_style()` before saving
+        or showing a figure, the font style "SimHei" will apply to
+        all the text in a figure. This type of font style supports
+        Chinese characters. If you prefer other font styles, just
+        give where it's stored as an input argument to `plt_set_font_style()`,
+        and all the text in the figure will be presented in that style.
+
         Args:
             font_style_path (path of font style):
                 If None, ``simhei.ttf`` will be used as default font style.
