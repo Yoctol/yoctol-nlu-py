045c18029f5e7bd4bcfc8df72da55f8db9997e73
Solumilken <s916526000@gmail.com>
2018-04-10 17:52:23 +0800

    added docstrings

--- ynlu/sdk/evaluation/entity_confusion_matrix.py
+++ ynlu/sdk/evaluation/entity_confusion_matrix.py
@@ -12,6 +12,55 @@ def entity_confusion_matrix(
         entity_predictions: List[List[dict]],
         y_trues: List[List[str]],
     ) -> Tuple[np.ndarray, List[str]]:
+    """Compute confusion matrix to evaluate the accuracy of a classification
+
+    Preprocess a list of raw entity predictions and
+    compute confusion matrix by calling ``sklearn.metrics.confusion_matrix``.
+
+    Args:
+        utterances (a list of strings):
+            The original input that produce the entity_predictions below
+            by calling `model.predict()`.
+        entity_predictions (a list of entity_predictions):
+            Entity part of output when callng `model.predict()` with the above
+            utterance as input.
+        y_trues (a list of y_true):
+            A list of true entity lists of that utterance.
+
+    Returns:
+        confusion_matrix (numpy 2D array):
+            By definition a confusion matrix C is such that :math:`C_{i, j}` is equal to the
+            number of observations known to be in group i but predicted to be in group j.
+            If a confusion matrix is a diagonal matrix, we can know that the predictions
+            and true labels are perfectly matched.
+
+        unique_entities (list of strings):
+            It records the meaning of group :math:`i`, :math:`j` of confusion matrix :math:`C_{i, j}`.
+
+    Examples:
+        >>> from ynlu.sdj.evaluation import entity_confusion_matrix
+        >>> confusion_matrix, unique_entities = entity_confusion_matrix(
+                utterances=["I like apple."],
+                entity_predictions=[
+                    [
+                        {"entity": "DONT_CARE", "value": "I like ", "score": 0.9},
+                        {"entity": "fruit", "value": "apple", "score": 0.8},
+                        {"entity": "drink", "value": ".", "score": 0.3},
+                    ],
+                ],
+                y_trues=[
+                    [
+                        "DONT_CARE", "DONT_CARE", "DONT_CARE", "DONT_CARE",
+                        "DONT_CARE", "DONT_CARE", "DONT_CARE", "fruit",
+                        "fruit", "fruit", "fruit", "fruit", "DONT_CARE",
+                    ],
+                ],
+            )
+        >>> print(unique_entities)
+        ["DONT_CARE", "fruit", "drink"]
+        >>> print(confusion_matrix)
+        np.array([[7, 0, 1], [0, 5, 0], [0, 0, 0]])
+    """
     flatten_y_pred = []
     flatten_y_true = []
     for utt, pred, y_true in zip(utterances, entity_predictions, y_trues):
--- ynlu/sdk/evaluation/utils.py
+++ ynlu/sdk/evaluation/utils.py
@@ -16,6 +16,32 @@ def preprocess_annotated_utterance(
         annotated_utterance: str,
         not_entity: str = NOT_ENTITY,
     ) -> List[str]:
+    """Character Level Entity Label Producer
+
+        Named-entity of each character is extracted by XML-like annotation.
+        Also, they would be collected in a list conform to the order of characters
+        in the sentence.
+
+        Args:
+            annotated_utterance (a string):
+                An utterance with annotations looks like <a>blabla</a>.
+                It is a special format for labeling named-entity in an utterance.
+            not_entity (a string, default = "DONT_CARE"):
+                A representation of words that we don't care about.
+
+        Returns:
+            entities (a list of string):
+                A list of named-entity labels in character level.
+
+        Examples:
+            >>> from ynlu.sdk.evaluation.utils import preprocess_annotated_utterance
+            >>> preprocess_annotated_utterance(
+                annotated_utterance="<drink>Coffee</drink>, please.",
+                not_entity="n",
+            )
+            >>> ["drink", "drink", "drink", "drink", "drink", "drink", "n",
+                "n", "n", "n", "n", "n", "n", "n", "n"]
+    """
     clean_utterance = remove_annotation(annotated_utterance)
     entity_word_pair = FINDALL_PROG.findall(annotated_utterance)
     entities = [not_entity] * len(clean_utterance)
@@ -36,6 +62,40 @@ def preprocess_entity_prediction(
         entity_prediction: List[dict],
         not_entity: str = NOT_ENTITY,
     )-> List[str]:
+    """Character Level Entity Label Producer
+
+        Named-entity of each character is extracted by the output of model prediction.
+        Also, they would be collected in a list conform to the order of characters
+        in the sentence.
+
+        Args:
+            utterance (a string):
+                An input of `model.predict()`.
+            entity_prediction (a list dictionaries):
+                Entity part of output returns by calling `model.predict()` with
+                the utterance above as input. The element in the list is a segment
+                of utterance, the predicted entity type and the confidence of
+                prediction.
+            not_entity (a string, default = "DONT_CARE"):
+                A representation of words that we don't care about.
+
+        Returns:
+            entities (a list of string):
+                A list of named-entity labels in character level.
+
+        Examples:
+            >>> from ynlu.sdk.evaluation.utils import preprocess_annotated_utterance
+            >>> preprocess_entity_prediction(
+                    utterance="Coffee, please.",
+                    entity_prediction=[
+                        {"value": "Coffee", "entity": "drink", "score": 0.8},
+                        {"value": ", please.", "entity": "n"}, "score": 0.7},
+                    ],
+                    not_entity="n",
+                )
+            >>> ["drink", "drink", "drink", "drink", "drink", "drink", "n",
+                "n", "n", "n", "n", "n", "n", "n", "n"]
+    """
     entities = [not_entity] * len(utterance)
     begin_index = 0
     for pred in entity_prediction:
@@ -57,6 +117,36 @@ def preprocess_intent_prediction_by_threshold(
         threshold: float = 0.5,
         unknown_token: str = UNKNOWN,
     ) -> List[str]:
+    """Predicted Intent Overrider
+
+        Override the predicted intent with the unknown token when its score is
+        lower than the threshold.
+
+        Args:
+            intent_predictions( a list of intent predictions):
+                A list of intent part of output by calling `model.predict()`.
+            threshold (float, defualt is 0.5):
+                The indicator about whether to override the prediciton or not.
+            unknown_token (a string, default is UNk):
+                A token which would be used as an alternative to a lower-confidence
+                intent prediction.
+
+        Returns:
+            output (a list of string):
+                A list of preprocessed intent labels.
+
+        Examples:
+            >>> from ynlu.sdk.evaluation.utils import preprocess_intent_prediction_by_threshold
+            >>> preprocess_intent_prediction_by_threshold(
+                    intent_predictions=[
+                        [{"intent": "a", "score": 0.3}, {"intent": "b", "score": 0.1}],
+                        [{"intent": "b", "score": 0.7}, {"intent": "c", "score": 0.3}],
+                    ]
+                    threshold=0.5,
+                    unknown_token="oo",
+                )
+            >>> [["oo", "oo"], ["b", "oo"]]
+    """
     output = []
     for intent_pred in intent_predictions:
         if intent_pred[0]["score"] > threshold:
